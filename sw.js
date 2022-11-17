2b024c3495ee4521038804a2a35bd1e00d79ecc0
Alessandro Sebastiani <sebbalex@gmail.com>
2022-11-17 19:52:46 +0100

    fix: seo

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c23b52535920740b30c2.js"
+    "url": "webpack-runtime-40484a17cd4f1499debe.js"
   },
   {
     "url": "framework-c813775b34b329f18251.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ae266c09fa10361105e5da36afb055b7"
+    "revision": "dabcad1c669cd5013c7d6fed760f1597"
   },
   {
     "url": "polyfill-494a98134aec3687172b.js"
